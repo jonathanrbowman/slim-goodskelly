ebf158d356aa0eaadd9515c3e84257972cc48282
Jonathan Bowman <bowman@swiftbunny.com>
2021-07-31 17:10:45 -0400

    Added gtag

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
 
-Vue.config.productionTip = false
+Vue.use(VueGtag, {
+  config: { id: "G-QD9SFMCBEL" }
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
